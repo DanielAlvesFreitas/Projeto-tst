e9ecdfd6a75333ce7cf9e054f8fb56a071f8d3f0
Daniel <Daniel@192.168.0.100>
2020-05-10 14:13:29 -0300

    Meu primeiro Commit

---
+++ src/pct/Ola.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pct;
+
+/**
+ *
+ * @author manager
+ */
+public class Ola {
+    public static void main(String[] args) {
+        //Escrevendo 'Boa Tarde à Todos'
+        System.out.println("Boa Tarde à Todos!");
+        
+    }
+    
+    
+    
+}
